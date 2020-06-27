bd160568a4953eaf9d12601d7ef94c1d30da8b69
baba_s <shotababa1206@gmail.com>
2020-06-27 12:47:21 +0900

    first commit

---
+++ Editor/SceneHierarchyInternal.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace Kogane
+{
+	public static class SceneHierarchyInternal
+	{
+		public static void RenameGO()
+		{
+			var assembly                 = typeof( EditorWindow ).Assembly;
+			var sceneHierarchyWindowType = assembly.GetType( "UnityEditor.SceneHierarchyWindow" );
+			var sceneHierarchyType       = assembly.GetType( "UnityEditor.SceneHierarchy" );
+			var sceneHierarchyField      = sceneHierarchyWindowType.GetField( "m_SceneHierarchy", BindingFlags.Instance | BindingFlags.NonPublic );
+			var renameGO                 = sceneHierarchyType.GetMethod( "RenameGO", BindingFlags.Instance | BindingFlags.NonPublic );
+			var sceneHierarchyWindow     = EditorWindow.GetWindow( sceneHierarchyWindowType );
+			var sceneHierarchy           = sceneHierarchyField.GetValue( sceneHierarchyWindow );
+
+			renameGO.Invoke( sceneHierarchy, null );
+		}
+	}
+}
